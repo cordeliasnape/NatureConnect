ced4c66476e5516011ceee1f260c2b47eab5f5bb
cordelia <snapecordelia@gmail.com>
2024-02-06 17:14:55 +0000

    update/ backend started and location started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,32 @@
+import React, { useState } from "react";
 import "./App.css";
 
-function Home() {
+function App() {
+  const [locationInput, setLocationInput] = useState("");
+
+  const handleLocationInputChange = (event) => {
+    setLocationInput(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Location input:", locationInput);
+  };
+
   return (
     <main>
-      <h1>Nature Connect</h1>
+      <h1>NatureConnect</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="locationInput">What's your location?</label>
+        <input
+          type="text"
+          value={locationInput}
+          onChange={handleLocationInputChange}
+        />
+        <button type="submit">Go!</button>
+      </form>
     </main>
   );
 }
 
-export default Home;
+export default App;
